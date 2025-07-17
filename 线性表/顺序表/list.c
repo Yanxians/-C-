fbfa45faa562d3ebcadca0712ef03eab8b1368da
Yanxians <97447243+Yanxians@users.noreply.github.com>
2025-07-17 17:51:16 +0800

    list.c

--- 线性表/顺序表/list.c
+++ 线性表/顺序表/list.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+//折半查找法，返回下标
+int binarysearch(int a[], int target, int low, int high){
+    if(high < low) return -1;
+    int mid = (low+high)/2;
+    if(a[mid] == target)
+    return mid;
+    if(a[mid] > target){
+        high = mid -1 ;
+        return binarysearch(a, target,low,high);
+    }
+    else if{
+        low = mid + 1;
+        return binarysearch(a, target,low,high);
+    }
+}
+
 #define MAXSIZE 100
 
 typedef struct SList{
@@ -37,4 +53,22 @@ bool Insert(List *L,int i, int e){
         L->data[j+1] = L->data[j];
     }
     L->data[i-1] = e;
+    L->length--;
 }
+
+bool Delete(List *L,i,int *e){
+    *e = L->p[i-1];
+    for(int j =i;j<L->length-1;j++){
+        L->p[j-1] = L->p[j]; 
+    }
+    L->length --;
+}
+
+int Locate(List *L, int e){
+    return binarysearch[L->p, e, 0, L->length-1];
+}
+
+int Get(List *L,int i){
+    return L->p[i-1];
+}
+
